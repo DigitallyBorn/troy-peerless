d6a7f41d159c169852cc0e15c750d0336a33555f
Ricky Smith <ricky.smith@meograph.com>
2015-08-22 15:52:50 -0400

    Production tweaks

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,6 @@ module TroyPeerless
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Force SSL for production
-    config.force_ssl = Rails.env.production?
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
